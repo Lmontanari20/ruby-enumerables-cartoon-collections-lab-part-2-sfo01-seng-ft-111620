80741701ae03550f2588e470c3304fceacbe2f66
Lucas Montanari <lmonty2011@gmail.com>
2020-10-29 19:34:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   result = false
   planeteer_calls.each {|n| 
-    if n.length > 4
+    if n.size > 4
       result = false
     end
   }
